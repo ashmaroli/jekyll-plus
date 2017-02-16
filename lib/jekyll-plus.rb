1bc3e0c15d7ff3d7ea4977d0d0637a2fc92a252b
Ashwin Maroli <ashmaroli@gmail.com>
2017-02-16 12:38:15 +0530

    patch Mercenary's Presenter and Option classes

--- lib/jekyll-plus.rb
+++ lib/jekyll-plus.rb
@@ -9,7 +9,12 @@ require "jekyll-data" # read "_config.yml" and data files within a theme-gem
 # ------------------------------ Temporary Patches ------------------------------
 # TODO:
 #   - remove patch to Jekyll Configuration after jekyll/jekyll/pull/5487 is merged.
+#   - modify patch to Mercenary after jekyll/mercenary/pull/44 and
+#     jekyll/mercenary/pull/48 are merged.
 
 require_relative "patches/idempotent_jekyll_config"
+
+require "mercenary"
+require_relative "patches/mercenary_presenter"
 
 # --------------------------- End of Temporary Patches ---------------------------
