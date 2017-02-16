8e918472d01edfed52683227834a0f57a8a289dc
Ashwin Maroli <ashmaroli@gmail.com>
2017-02-16 12:42:25 +0530

    patch `Jekyll::Watcher`

--- lib/jekyll-plus.rb
+++ lib/jekyll-plus.rb
@@ -11,10 +11,14 @@ require "jekyll-data" # read "_config.yml" and data files within a theme-gem
 #   - remove patch to Jekyll Configuration after jekyll/jekyll/pull/5487 is merged.
 #   - modify patch to Mercenary after jekyll/mercenary/pull/44 and
 #     jekyll/mercenary/pull/48 are merged.
+#   - remove patch to Jekyll::Watcher after jekyll/jekyll-watch/pull/42 is merged.
 
 require_relative "patches/idempotent_jekyll_config"
 
 require "mercenary"
 require_relative "patches/mercenary_presenter"
+
+require "jekyll-watch"
+require_relative "patches/jekyll_watcher"
 
 # --------------------------- End of Temporary Patches ---------------------------
