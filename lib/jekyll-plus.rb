7558d3077e384ed70529c6b66b298faed5bda87c
Ashwin Maroli <ashmaroli@gmail.com>
2017-02-16 12:47:46 +0530

    patch Listen's Adapter for Windows
    remove calling for gem "wdm" till someone reports an issue with this

--- lib/jekyll-plus.rb
+++ lib/jekyll-plus.rb
@@ -12,6 +12,7 @@ require "jekyll-data" # read "_config.yml" and data files within a theme-gem
 #   - modify patch to Mercenary after jekyll/mercenary/pull/44 and
 #     jekyll/mercenary/pull/48 are merged.
 #   - remove patch to Jekyll::Watcher after jekyll/jekyll-watch/pull/42 is merged.
+#   - remove patch to Listen Adapter only if any issues regarding that crop up.
 
 require_relative "patches/idempotent_jekyll_config"
 
@@ -20,5 +21,8 @@ require_relative "patches/mercenary_presenter"
 
 require "jekyll-watch"
 require_relative "patches/jekyll_watcher"
+
+require "listen"
+require_relative "patches/listen_windows_adapter"
 
 # --------------------------- End of Temporary Patches ---------------------------
---
+++ lib/patches/listen_windows_adapter.rb
@@ -0,0 +1,8 @@
+module Listen
+  module Adapter
+    class Windows < Base
+      def self.usable?
+      end
+    end
+  end
+end
